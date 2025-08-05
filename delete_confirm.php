1dba5e007405bdff58baa11885d1cc342fb9e688
grsvro <gakoyo725@gmail.com>
2025-08-05 15:17:12 +0900

    delete_confirm

--- delete_confirm.php
+++ delete_confirm.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/delete_confirm.css">
+    <!-- git pull origin main コミットする前のお約束 -->
     <title>Document</title>
 </head>
 
